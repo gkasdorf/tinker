b0ff8b685df179e7ba35f1878656eee0564cf178
Gavin Kasdorf <me@gkasdorf.com>
2023-07-13 16:28:29 -0700

    initial

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ReactMarkdown from "react-markdown";
 
 export default function App() {
+  const text = `
+  # Header
+  ## Header two
+  
+  Some text
+  
+  *italic*
+  
+  **bold**
+  `
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text><ReactMarkdown children={text} components={{p: Text, h1: Hone, h2: Htwo, strong: Htwo, em: Htwo}} /></Text>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function Hone({children}) {
+  return (
+    <Text style={{fontSize: 30}}>
+      {children}
+    </Text>
+  );
+}
+
+function Htwo({children}) {
+  return (
+    <Text style={{fontSize: 20}}>
+      {children}
+    </Text>
   );
 }
 
